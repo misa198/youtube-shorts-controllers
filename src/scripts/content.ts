a3467787f36813d40cef74428b99565e198e66d0
Thanh Vu <thanhvc.198@gmail.com>
2022-04-26 00:24:32 +0700

    fix: fix ext not working when youtube.com -> youtube.com/shorts

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -21,117 +21,122 @@ const start = async () => {
   observe('#shorts-container', {
     add(shortContainer) {
       const shortContainerObserver = new SelectorObserver(shortContainer);
-      shortContainerObserver.observe('#player-container', {
-        add(playerContainer) {
-          playerContainer.insertAdjacentHTML('beforeend', controllerHtml);
-          const playerContainerObserver = new SelectorObserver(playerContainer);
-          const playButton = playerContainer.querySelector(
-            'button.shorts-controller__button--play'
-          ) as HTMLButtonElement;
-          const playPauseIcon = playButton.querySelector(
-            '.play-pause-icon-path'
-          ) as HTMLButtonElement;
-          const speakerIconPath = playerContainer.querySelector(
-            '.speaker-icon-path'
-          ) as HTMLButtonElement;
-          const muteButton = playerContainer.querySelector(
-            '.mute-button'
-          ) as HTMLButtonElement;
-          const forwardButton = playerContainer.querySelector(
-            '.forward-button'
-          ) as HTMLButtonElement;
-          const rewindButton = playerContainer.querySelector(
-            '.rewind-button'
-          ) as HTMLButtonElement;
-          const processbarThumb = playerContainer.querySelector(
-            '.processbar__thumb'
-          ) as HTMLDivElement;
+      shortContainerObserver.observe(
+        '.reel-video-in-sequence.style-scope.ytd-shorts > #player-container',
+        {
+          add(playerContainer) {
+            playerContainer.insertAdjacentHTML('beforeend', controllerHtml);
+            const playerContainerObserver = new SelectorObserver(
+              playerContainer
+            );
+            const playButton = playerContainer.querySelector(
+              'button.shorts-controller__button--play'
+            ) as HTMLButtonElement;
+            const playPauseIcon = playButton.querySelector(
+              '.play-pause-icon-path'
+            ) as HTMLButtonElement;
+            const speakerIconPath = playerContainer.querySelector(
+              '.speaker-icon-path'
+            ) as HTMLButtonElement;
+            const muteButton = playerContainer.querySelector(
+              '.mute-button'
+            ) as HTMLButtonElement;
+            const forwardButton = playerContainer.querySelector(
+              '.forward-button'
+            ) as HTMLButtonElement;
+            const rewindButton = playerContainer.querySelector(
+              '.rewind-button'
+            ) as HTMLButtonElement;
+            const processbarThumb = playerContainer.querySelector(
+              '.processbar__thumb'
+            ) as HTMLDivElement;
 
-          playerContainerObserver.observe('video', {
-            add(videoElement) {
-              videoElement.addEventListener('play', () => {
-                playPauseIcon.setAttribute('d', playIconD);
-                playButton.setAttribute('id', playIconId);
-              });
-              videoElement.addEventListener('pause', () => {
-                playPauseIcon.setAttribute('d', pauseIconD);
-                playButton.setAttribute('id', pauseIconId);
-              });
-              videoElement.addEventListener('volumechange', () => {
+            playerContainerObserver.observe('video', {
+              add(videoElement) {
+                videoElement.addEventListener('play', () => {
+                  playPauseIcon.setAttribute('d', playIconD);
+                  playButton.setAttribute('id', playIconId);
+                });
+                videoElement.addEventListener('pause', () => {
+                  playPauseIcon.setAttribute('d', pauseIconD);
+                  playButton.setAttribute('id', pauseIconId);
+                });
+                videoElement.addEventListener('volumechange', () => {
+                  if (_videoElement.muted) {
+                    speakerIconPath.setAttribute('d', mutedIconD);
+                    speakerIconPath.setAttribute('id', mutedIconId);
+                  } else {
+                    speakerIconPath.setAttribute('d', unmuteIconD);
+                    speakerIconPath.setAttribute('id', unmuteIconId);
+                  }
+                });
+
+                const _videoElement = videoElement as HTMLVideoElement;
+                videoElement.addEventListener('timeupdate', () => {
+                  const duration = _videoElement.duration;
+                  const currentTime = _videoElement.currentTime;
+                  const processbarWidth = (currentTime / duration) * 100 + '%';
+                  processbarThumb.style.width = processbarWidth;
+                });
+                if (_videoElement.paused) {
+                  playPauseIcon.setAttribute('d', pauseIconD);
+                  playPauseIcon.setAttribute('id', pauseIconId);
+                } else {
+                  playPauseIcon.setAttribute('d', playIconD);
+                  playPauseIcon.setAttribute('id', playIconId);
+                }
                 if (_videoElement.muted) {
                   speakerIconPath.setAttribute('d', mutedIconD);
-                  speakerIconPath.setAttribute('id', mutedIconId);
+                  speakerIconPath.setAttribute('id', pauseIconId);
                 } else {
                   speakerIconPath.setAttribute('d', unmuteIconD);
                   speakerIconPath.setAttribute('id', unmuteIconId);
                 }
-              });
 
-              const _videoElement = videoElement as HTMLVideoElement;
-              videoElement.addEventListener('timeupdate', () => {
-                const duration = _videoElement.duration;
-                const currentTime = _videoElement.currentTime;
-                const processbarWidth = (currentTime / duration) * 100 + '%';
-                processbarThumb.style.width = processbarWidth;
-              });
-              if (_videoElement.paused) {
-                playPauseIcon.setAttribute('d', pauseIconD);
-                playPauseIcon.setAttribute('id', pauseIconId);
-              } else {
-                playPauseIcon.setAttribute('d', playIconD);
-                playPauseIcon.setAttribute('id', playIconId);
-              }
-              if (_videoElement.muted) {
-                speakerIconPath.setAttribute('d', mutedIconD);
-                speakerIconPath.setAttribute('id', pauseIconId);
-              } else {
-                speakerIconPath.setAttribute('d', unmuteIconD);
-                speakerIconPath.setAttribute('id', unmuteIconId);
-              }
-
-              playButton.addEventListener('click', () => {
-                if (_videoElement.paused) {
-                  _videoElement.play();
-                } else {
-                  _videoElement.pause();
-                }
-              });
-
-              muteButton.addEventListener('click', () => {
-                if (_videoElement.muted) {
-                  _videoElement.muted = false;
-                } else {
-                  _videoElement.muted = true;
-                }
-              });
-
-              forwardButton.addEventListener('click', () => {
-                get([TIME_STEP_KEY], (items) => {
-                  _videoElement.currentTime +=
-                    parseInt(items[TIME_STEP_KEY], 10) || 5;
+                playButton.addEventListener('click', () => {
+                  if (_videoElement.paused) {
+                    _videoElement.play();
+                  } else {
+                    _videoElement.pause();
+                  }
                 });
-              });
-              rewindButton.addEventListener('click', () => {
-                get([TIME_STEP_KEY], (items) => {
-                  _videoElement.currentTime -=
-                    parseInt(items[TIME_STEP_KEY], 10) || 5;
-                });
-              });
 
-              playerContainer.addEventListener('keydown', (e) => {
-                switch ((e as KeyboardEvent).key) {
-                  case 'ArrowLeft':
-                    rewindButton.click();
-                    break;
-                  case 'ArrowRight':
-                    forwardButton.click();
-                    break;
-                }
-              });
-            },
-          });
-        },
-      });
+                muteButton.addEventListener('click', () => {
+                  if (_videoElement.muted) {
+                    _videoElement.muted = false;
+                  } else {
+                    _videoElement.muted = true;
+                  }
+                });
+
+                forwardButton.addEventListener('click', () => {
+                  get([TIME_STEP_KEY], (items) => {
+                    _videoElement.currentTime +=
+                      parseInt(items[TIME_STEP_KEY], 10) || 5;
+                  });
+                });
+                rewindButton.addEventListener('click', () => {
+                  get([TIME_STEP_KEY], (items) => {
+                    _videoElement.currentTime -=
+                      parseInt(items[TIME_STEP_KEY], 10) || 5;
+                  });
+                });
+
+                playerContainer.addEventListener('keydown', (e) => {
+                  switch ((e as KeyboardEvent).key) {
+                    case 'ArrowLeft':
+                      rewindButton.click();
+                      break;
+                    case 'ArrowRight':
+                      forwardButton.click();
+                      break;
+                  }
+                });
+              },
+            });
+          },
+        }
+      );
     },
   });
 };
